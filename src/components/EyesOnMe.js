fe458462cc9c49114c7fbd3df79b446520e7e6bd
github username <github email address>
2018-11-04 00:16:12 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+    constructor() {
+        super();
+        this.state= {
+          regarde: false
+        }
+    }
+
+    handleClick= () => {
+        this.setState({regarde: !this.state.regarde})
+        if (!this.state.regarde){
+            console.log('Good!')
+        } 
+        else {
+            console.log('Hey! Eyes on me!')
+        }
+    };
+
+    render(){
+        return <button onClick={this.handleClick}>{!this.state.regarde ? "Focus"  : "Blur" }</button>
+    }
+}
+
+export default EyesOnMe
